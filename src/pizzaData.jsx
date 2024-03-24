70c575617ce80918fb7a1d0051919f6057cd2019
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-03-23 22:53:39 -0300

    Fix: size of main components

--- src/pizzaData.jsx
+++ src/pizzaData.jsx
@@ -1,50 +1,50 @@
 const pizzasData = [
     {
         id: 1,
-        name: "Pizza Muzarella",
+        name: "Mozzarella Pizza",
         price: 7.99,
         description:
-            "Classic pizza with tomato and mozzarella sauce.",
+            "Classic pizza topped with gooey mozzarella cheese and tangy tomato sauce.",
         ingredients: ["Tomate sauce", "Mozzarella"],
         image: "muzarella",
         stock: 10,
     },
     {
         id: 2,
-        name: "Pizza Margarita",
+        name: "Margarita Pizza",
         price: 8.99,
         description:
-            "Delicious pizza with tomato sauce, mozzarella and fresh basil.",
+            "Traditional pizza with tomato sauce, fresh mozzarella, and fragrant basil leaves.",
         ingredients: ["Tomate sauce", "Mozzarella", "Basil"],
         image: "margarita",
         stock: 10,
     },
     {
         id: 3,
-        name: "Pizza Pepperoni",
+        name: "Pepperoni Pizza",
         price: 9.99,
         description:
-            "Classic pizza with tomato sauce, mozzarella and pepperoni slices.",
+            "A favorite topped with zesty pepperoni slices and melted cheese on a tomato sauce base.",
         ingredients: ["Tomate sauce", "Mozzarella", "Pepperoni"],
         image: "pepperoni",
         stock: 15,
     },
     {
         id: 4,
-        name: "Pizza Fugazzeta",
+        name: "Fugazzeta Pizza",
         price: 9.99,
         description:
-            "Delicious pizza with tomato sauce, mozzarella and onion.",
+            " A unique pizza featuring a thick crust layered with mozzarella, onions, and a touch of oregano.",
         ingredients: ["Tomate sauce", "Mozzarella", "Onion"],
         image: "fugazzeta",
         stock: 8,
     },
     {
         id: 5,
-        name: "Pizza Napolitana",
+        name: "Napolitan Pizza",
         price: 8.99,
         description:
-            "Classic pizza with tomato sauce, mozzarella, tomato and oregano.",
+            " Authentic Italian pizza with a thin crust, topped with San Marzano tomato sauce, fresh buffalo mozzarella, and basil leaves.",
         ingredients: ["Tomate sauce", "Mozzarella", "Tomato", "Oregano"],
         image: "napolitana",
         stock: 8,
